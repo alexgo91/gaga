dac5c2a3b6a61e7086dd26e5e933742ed62a0f3c
Alex Gorbatovsky <alexgo91@gmail.com>
2026-02-04 17:52:08 -0600

    v1.0: init

---
+++ applications/gaga_app.common.js
@@ -0,0 +1,8 @@
+var app = {};
+app.isSPA = true;
+app.serviceManagerHost = '';			// to execute API calls on other server
+app.serviceCallTimeout = 60000;		// milliseconds to wait for service calls to finish
+app.serviceSuppressedParams = [];		// fields that won't be sent to service calls
+app.addLogoutToMenu = true;			// Adds logout as the last option to the menu (you must set login page for the application)
+app.autoFocusConfig = {};			    // Optional custom autofocus configuration
+app.loginPage = '';
